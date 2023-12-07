e94a56c53ce730d49a877c533bb41276cfd6d2e6
Tobias Watzek <spitzfaust@gmail.com>
2023-12-07 08:06:26 +0100

    2023: day 4 part 2 done

--- 2023/src/days/day4.rs
+++ 2023/src/days/day4.rs
@@ -1,8 +1,4 @@
-use std::{
-    collections::HashSet,
-    fs,
-    path::PathBuf,
-};
+use std::{collections::HashSet, fs, path::PathBuf, usize};
 
 pub fn solve(input_path: &PathBuf) -> (String, String) {
     let input = fs::read_to_string(input_path).expect("Should have been able to read the file");
@@ -48,6 +44,53 @@ fn solve_first(input: &str) -> i32 {
         .sum()
 }
 
-fn solve_second(_input: &str) -> i32 {
-    0
+fn solve_second(input: &str) -> usize {
+    let cards: Vec<Card> = input
+        .lines()
+        .filter(|l| !l.trim().is_empty())
+        .enumerate()
+        .map(|(id, l)| {
+            let card = l[8..]
+                .split('|')
+                .map(|nums| {
+                    nums.split(' ')
+                        .filter_map(|n| n.trim().parse::<i32>().ok())
+                        .collect::<HashSet<i32>>()
+                })
+                .collect::<Vec<_>>();
+            let (winning_numbers, scratched_numbers) = match &card[..] {
+                [first, second] => (first, second),
+                _ => unreachable!(),
+            };
+
+            let count = winning_numbers.intersection(&scratched_numbers).count();
+
+            Card {
+                id,
+                won_copies: (id + 1..id + count + 1).collect(),
+            }
+        })
+        .collect();
+
+    let sum_cards = cards
+        .iter()
+        .fold(0, |acc, card| acc + evaluate_cards(&card.id, &cards));
+
+    sum_cards
+}
+
+fn evaluate_cards(current_card: &usize, all_cards: &Vec<Card>) -> usize {
+    let mut count = 1;
+
+    for won_copy in &all_cards[*current_card].won_copies {
+        count += evaluate_cards(won_copy, all_cards);
+    }
+
+    count
+}
+
+#[derive(Debug)]
+struct Card {
+    id: usize,
+    won_copies: Vec<usize>,
 }
