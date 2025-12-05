180022016a981ea52f8242a7ca92ac5759f30c2d
Tobias Watzek <tobias@watzek.dev>
2025-12-05 20:50:50 +0100

    Add solution for day 3 part 2 2025

--- 2025/AoC2025/DayThree.cs
+++ 2025/AoC2025/DayThree.cs
@@ -5,24 +5,52 @@ public static class DayThree
     public static async Task Run()
     {
         var batteryBanks = await File.ReadLinesAsync("./inputs/03.txt")
-            .Select(line => line.Select(c => int.Parse(c.ToString())).ToArray())
+            .Select(line =>
+                line.Select(c => byte.Parse(c.ToString())).ToArray())
             .ToListAsync();
 
 
         SolvePartOne(batteryBanks);
+        SolvePartTwo(batteryBanks);
     }
 
-    private static void SolvePartOne(List<int[]> batteryBanks)
+    private static void SolvePartOne(List<byte[]> batteryBanks)
     {
         long totalJoltage = 0;
         foreach (var batteryBank in batteryBanks)
         {
-            var first = batteryBank[..^1].Select((battery, index) => (battery, index)).MaxBy(t => t.battery);
+            var first = batteryBank[..^1]
+                .Select((battery, index) => (battery, index))
+                .MaxBy(t => t.battery);
             var last = batteryBank[(first.index + 1)..].Max();
-            var joltage = int.Parse($"{first.battery}{last}");
+            var joltage = 10 * first.battery + last;
             totalJoltage += joltage;
         }
 
         Console.WriteLine($"Part One: {totalJoltage}");
+    }
+
+    private static void SolvePartTwo(List<byte[]> batteryBanks)
+    {
+        long totalJoltage = 0;
+
+        foreach (var batteryBank in batteryBanks)
+        {
+            var joltage = 0L;
+            var start = 0;
+            for (var position = 0; position < 12; position++)
+            {
+                var max = batteryBank[start..^(12 - (position + 1))]
+                    .Select((battery, index) => (battery, index: start + index))
+                    .MaxBy(t => t.battery);
+                joltage = 10 * joltage + max.battery;
+                start = max.index + 1;
+            }
+            
+            totalJoltage += joltage;
+        }
+
+
+        Console.WriteLine($"Part Two: {totalJoltage}");
     }
 }
--- 2025/AoC2025/Program.cs
+++ 2025/AoC2025/Program.cs
@@ -4,6 +4,6 @@ Console.WriteLine("AoC 2025");
 
 // await DayOne.Run();
 // await DayTwo.Run();
-// await DayThree.Run();
+ // await DayThree.Run();
 // await DayFour.Run();
 await DayFive.Run();
